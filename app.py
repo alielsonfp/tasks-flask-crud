f6742ef4f227c5c3ebebf03ab774b9fdc79185b8
alielsonfp <alielson_pequeno@hotmail.com>
2024-05-24 01:14:05 -0300

    ADD get method

--- app.py
+++ app.py
@@ -20,7 +20,18 @@ def create_task():
     print(tasks)
     return jsonify({"message": "Nova Tarefa criada com sucesso"})
 
-    
+@app.route('/tasks', methods=['GET'])
+
+def get_tasks():
+    task_list = []
+    for task in tasks:
+        task_list.append(task.to_dict())
+
+    output ={
+                "tasks": task_list,
+                "total_tasks": 0
+            }
+    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
